5a823b61d50ae1c60bcb7a68814773c74722c823
IdahoYouthRanch-IT <137314872+IdahoYouthRanch-IT@users.noreply.github.com>
2023-06-26 16:42:16 -0600

    Update WindowsUpdate_Uninstaller - test .ps1

--- WindowsUpdate_Uninstaller.ps1
+++ WindowsUpdate_Uninstaller.ps1
@@ -1,2 +1,2 @@
-# Replace the kb:nnnnnnn with the KB Number of the update to uninstall
+# Replace the kb:nnnnnnn with the KB Number of the update to uninstall - Steve Shapiro 2023-06
 wusa /uninstall /kb:nnnnnnn /quiet /forcerestart
