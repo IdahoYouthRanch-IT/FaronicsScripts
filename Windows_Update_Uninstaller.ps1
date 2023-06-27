16a58b31bbcd06463903a4dec3b7d600c7428936
IdahoYouthRanch-IT <137314872+IdahoYouthRanch-IT@users.noreply.github.com>
2023-06-27 10:32:26 -0600

    Update Windows_Update_Uninstaller.ps1

--- Windows_Update_Uninstaller.ps1
+++ Windows_Update_Uninstaller.ps1
@@ -1,5 +1,6 @@
-# Replace the KBnnnnnnn with the KB Number of the update to uninstall 
+# Replace nnnnnnn with the KB Number of the update to uninstall and commit the changes.
+# If the script has been previously used, update the KB digits with the KB number of the new update to be uninstalled.
 # Steve Shapiro 2023-06
 # Source: https://docs.faronics.com/faronicsdeploy/custom-scripts-powershell-vb-batch/custom-scripts-library/uninstall-a-windows-update
 #
-wusa /uninstall /KBnnnnnnn /quiet /forcerestart
+wusa /uninstall /KB:nnnnnnn /quiet /forcerestart
