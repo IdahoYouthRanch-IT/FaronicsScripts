803d0464ccf2ee919cf4920ea88156e6f5aa3e01
IdahoYouthRanch-IT <137314872+IdahoYouthRanch-IT@users.noreply.github.com>
2024-10-01 15:22:41 -0600

    Add files via upload

---
+++ RestartComputer.ps1
@@ -0,0 +1,15 @@
+﻿# Define the target time (e.g., 15:30)
+$targetTime = Get-Date -Hour 15 -Minute 09
+
+# Loop until the current time reaches the target time
+while ((Get-Date) -lt $targetTime) {
+    Start-Sleep -Seconds 5
+}
+
+# Execution will resume once the target time is reached
+
+Write-Host "Resuming execution now."
+
+Add-Type -AssemblyName PresentationFramework
+[System.Windows.MessageBox]::Show('Hello, this is a pop-up message')
+# Restart-Computer -ComputerName IT-2041 -Force
