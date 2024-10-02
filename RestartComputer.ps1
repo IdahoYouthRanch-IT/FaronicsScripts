0894879cc96132b12ac1668028d427d0d02453b9
IdahoYouthRanch-IT <137314872+IdahoYouthRanch-IT@users.noreply.github.com>
2024-10-02 15:09:56 -0600

    Update RestartComputer.ps1

--- RestartComputer.ps1
+++ RestartComputer.ps1
@@ -2,5 +2,6 @@ $targetTime = Get-Date -Hour 11 -Minute 55
 while ((Get-Date) -lt $targetTime) {
     Start-Sleep -Seconds 5
 }
-msg.exe * "Hello from Faronics"
-# Restart-Computer -ComputerName IT-2041 -Force
+msg.exe * "about to restart"
+Start-Sleep -Seconds 10
+Restart-Computer -Force
