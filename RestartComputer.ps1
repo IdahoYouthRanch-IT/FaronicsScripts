719f59c2a6256451c9635cd8d2b54ec5799b057b
IdahoYouthRanch-IT <137314872+IdahoYouthRanch-IT@users.noreply.github.com>
2024-10-02 11:56:03 -0600

    Update RestartComputer.ps1

--- RestartComputer.ps1
+++ RestartComputer.ps1
@@ -1,13 +1,6 @@
-﻿# Define the target time (e.g., 15:30)
 $targetTime = Get-Date -Hour 11 -Minute 55
-
-# Loop until the current time reaches the target time
 while ((Get-Date) -lt $targetTime) {
     Start-Sleep -Seconds 5
 }
-
-# Execution will resume once the target time is reached
-
 msg.exe * "Hello from Faronics"
-
 # Restart-Computer -ComputerName IT-2041 -Force
