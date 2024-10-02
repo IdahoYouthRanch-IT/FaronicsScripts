6c94f2531b8b427160355b772df4b5fbb0786eff
IdahoYouthRanch-IT <137314872+IdahoYouthRanch-IT@users.noreply.github.com>
2024-10-02 16:34:42 -0600

    Delete RestartComputer.ps1

--- RestartComputer.ps1
+++
@@ -1,7 +0,0 @@
-$targetTime = Get-Date -Hour 15 -Minute 20
-while ((Get-Date) -lt $targetTime) {
-    Start-Sleep -Seconds 5
-}
-msg.exe * "about to restart"
-Start-Sleep -Seconds 10
-Restart-Computer -Force
