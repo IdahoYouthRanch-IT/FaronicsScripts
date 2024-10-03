61bbf0e4ccc39b2e9575bae73c550792b9539db0
IdahoYouthRanch-IT <137314872+IdahoYouthRanch-IT@users.noreply.github.com>
2024-10-03 12:08:02 -0600

    Update DisplayWarningThenRestart.ps1

--- DisplayWarningThenRestart.ps1
+++ DisplayWarningThenRestart.ps1
@@ -1,5 +1,5 @@
 # Define the target time (e.g., 15:30)
-$targetTime = Get-Date -Hour 11 -Minute 36
+$targetTime = Get-Date -Hour 12 -Minute 10
 
 # Loop until the current time reaches the target time
 while ((Get-Date) -lt $targetTime) {
