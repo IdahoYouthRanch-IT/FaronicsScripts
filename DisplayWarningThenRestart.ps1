a9f06a327c63b04add83f3c188347f4913ace45d
IdahoYouthRanch-IT <137314872+IdahoYouthRanch-IT@users.noreply.github.com>
2024-10-03 11:33:35 -0600

    Update DisplayWarningThenRestart.ps1

--- DisplayWarningThenRestart.ps1
+++ DisplayWarningThenRestart.ps1
@@ -1,35 +1,15 @@
 # Define the target time (e.g., 15:30)
-$targetTime = Get-Date -Hour 11 -Minute 30
+$targetTime = Get-Date -Hour 11 -Minute 36
 
 # Loop until the current time reaches the target time
 while ((Get-Date) -lt $targetTime) {
     Start-Sleep -Seconds 5
-} 
- 
-Add-Type -AssemblyName System.Windows.Forms    
+}
 
-# Build Form
-    $objForm = New-Object System.Windows.Forms.Form
-    $objForm.Text = "Test"
-    $objForm.Size = New-Object System.Drawing.Size(800,500)
+# Execution will resume once the target time is reached
 
-# Add Label
-    $objLabel = New-Object System.Windows.Forms.Label
-    $objLabel.Location = New-Object System.Drawing.Size(300,150) 
-    $objLabel.Size = New-Object System.Drawing.Size(300,300)
-    $objLabel.Font = New-Object System.Drawing.Font("Arial",30)
-    $objLabel.Text = "Your computer will restart in 30 seconds. Please save your work"
-    $objForm.Controls.Add($objLabel)
+msg.exe * "Your computer is about to restart. Please save your work"
 
-# Show the form
-    $objForm.Show()| Out-Null
+Start-Sleep -Seconds 15
 
-# wait 30 seconds
-    Start-Sleep -Seconds 30
-    
-# destroy form
-    $objForm.Close() | Out-Null
-
-#Restart Computer
-    Start-Sleep -Seconds 2
-  #  restart-Computer -force
+Restart-Computer -Force
